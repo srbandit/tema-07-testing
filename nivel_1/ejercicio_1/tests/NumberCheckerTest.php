cee9786799d0a82991d164b08cad2bb5da756ce3
srbandit <149817789+srbandit@users.noreply.github.com>
2024-05-06 17:38:25 +0200

    ejercicio 1

---
+++ nivel_1/ejercicio_1/tests/NumberCheckerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class NumberCheckerTest extends TestCase{
+
+    public function testChequeaNumeroPositivo(){
+        $numero = new NumberChecker(10);
+        $this->assertTrue($numero->isPositive(), "Es positivo");
+        
+    }
+
+    public function testChequeaNumeroPrimo(){
+        $numero2 = new NumberChecker(2);
+        $this->assertTrue($numero2->isEven()
+        , "es primo");
+    }
+}
+
+
+?>
